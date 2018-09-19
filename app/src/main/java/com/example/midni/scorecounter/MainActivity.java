73863f00b5975839b10015f91c94682d200b8328
Chanice <chanicecol@yahoo.com>
2018-09-19 15:58:38 -0400

    working buttons

--- app/src/main/java/com/example/midni/scorecounter/MainActivity.java
+++ app/src/main/java/com/example/midni/scorecounter/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.midni.scorecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView Team1Score;
     private TextView Team2Score;
@@ -23,23 +24,29 @@ public class MainActivity extends AppCompatActivity {
         Team2Score = findViewById(R.id.teamTwoScore);
         increase1 = findViewById(R.id.teamOneButton);
         increase2 = findViewById(R.id.teamTwoButton);
+        increase1.setOnClickListener((View.OnClickListener)this);
+        increase2.setOnClickListener((View.OnClickListener)this);
 
         Team1Score.setText(""+count1);
         Team2Score.setText(""+count2);
 
     }
     public void onClick(View view){
+        Log.d(TAG,"ONCLICK");
         if(increase1.equals(view)){
+
             count1++;
-            updateScore();
+            Team1Score.setText(""+count1);
         }
     else if(increase2.equals(view)){
+            Log.d(TAG,"score2 increase");
             count2++;
             updateScore();
         }
     }
     public void updateScore(){
-        Team1Score.setText(""+count1);
+        Log.d(TAG,"UPDATE SCORE METHOD");
+
         Team2Score.setText(""+count2);
     }
 
