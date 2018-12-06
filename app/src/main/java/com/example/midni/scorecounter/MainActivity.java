688f512891eba45456e37529a57d87d06993e204
Chanice <chanicecol@yahoo.com>
2018-12-06 00:44:28 -0500

    working settings menu

--- app/src/main/java/com/example/midni/scorecounter/MainActivity.java
+++ app/src/main/java/com/example/midni/scorecounter/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.midni.scorecounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
@@ -35,6 +37,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Team1Score.setText(""+count1);
         Team2Score.setText(""+count2);
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
     public void onClick(View view){
         Log.d(TAG,"ONCLICK");
